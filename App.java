ec52d175beaa0038430f08bc77f5682733d8af0c
lbarjak <barjak.laszlo@gmail.com>
2023-02-16 19:11:23 +0100

    init

---
+++ App.java
@@ -0,0 +1,13 @@
+package eu.barjak.java.MartonOmsz;
+
+import java.io.IOException;
+
+public class App 
+{
+    public static void main( String[] args ) throws IOException
+    {
+        //System.out.println( "Hello World!" );
+    	new WeatherQuery().query();
+
+    }
+}
