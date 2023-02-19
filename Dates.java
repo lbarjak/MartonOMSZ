de2613e0afdb42ff78687af00f891ccb997a3d49
lbarjak <barjak.laszlo@gmail.com>
2023-02-19 19:10:16 +0100

    locale "hu"

--- Dates.java
+++ Dates.java
@@ -1,13 +1,31 @@
 package eu.barjak.java.MartonOmsz;
 
+import java.time.DayOfWeek;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
 import java.util.Date;
+import java.util.Locale;
 
 public class Dates {
+	
+	Locale huLoc = new Locale("hu");
     
     public String now(int difference) {
     	LocalDate now = LocalDate.now();
-    	return now.plusDays(difference).toString();
+    	System.out.println(Instant.now());
+    	
+    	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");  
+    	System.out.println(dtf.format(LocalDateTime.now()));
+    	dtf = DateTimeFormatter.ofPattern("HH:mm");  
+    	System.out.println(dtf.format(LocalDateTime.now()));
+    	
+    	DayOfWeek dow = now.getDayOfWeek();
+    	String dayName = dow.getDisplayName(TextStyle.FULL, huLoc);
+    	System.out.println(dayName);
+    	return (now.plusDays(difference).toString());
     }
     public void diff(Date start, Date stop) {
         long diffSec = (stop.getTime() - start.getTime()) / 1000;
