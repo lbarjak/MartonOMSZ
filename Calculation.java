6398a31424bb6304aa193208ecbad3b8aed86783
lbarjak <barjak.laszlo@gmail.com>
2023-02-22 21:59:16 +0100

    last 24h average

--- Calculation.java
+++ Calculation.java
@@ -39,10 +39,22 @@ public class Calculation implements GlobalVariables {
 			temperature.setRoomTemp2(roomTemp2);
 			roomTemp1 = roomTemp2;
 		}
+		
+		last24Average();
 	}
 	
 	public void tau() {
 		roomTemp2 = outdoorTemp + (roomTemp1 - outdoorTemp) * multiplier;
+	}
+	
+	public void last24Average() {
+		Double sum = 0.0;
+		Double temp = 0.0;
+		for(int i = TEMPERATURES.size() - 1; i >= TEMPERATURES.size() - 144; i--) {
+			temp = TEMPERATURES.get(i).getOutdoorTemp();
+			sum+= temp;
+		}
+		System.out.println("Az utolsó 24 óra átlaga: " + sum/144);
 	}
 
 }
